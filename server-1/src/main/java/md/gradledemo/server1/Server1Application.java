ea8ba3a81d36e89d65e1f0ab70d8c4891d8eea47
MarekDudek <marek.dudek@gmail.com>
2023-02-16 23:12:08 +0100

    library used in server

--- server-1/src/main/java/md/gradledemo/server1/Server1Application.java
+++ server-1/src/main/java/md/gradledemo/server1/Server1Application.java
@@ -1,5 +1,7 @@
 package md.gradledemo.server1;
 
+import jakarta.annotation.PostConstruct;
+import md.gradledemo.library1.Library1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,11 @@ public class Server1Application
     public static void main(String[] args)
     {
         SpringApplication.run(Server1Application.class, args);
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        Library1.method1();
     }
 }
